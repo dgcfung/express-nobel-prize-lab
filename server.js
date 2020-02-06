bba090477b67d3397f3c34cc0713eedc7e0e6a13
Steven VanWoerkom <vanwoerkom.steve@gmail.com>
2020-02-06 09:37:30 -0500

    blank server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const { Prize } = require('./models');
-
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.json({msg: 'This is the root!'});
-});
-
-
-app.listen(3000, () => console.log('up and running on: '));
+// Build server here!
